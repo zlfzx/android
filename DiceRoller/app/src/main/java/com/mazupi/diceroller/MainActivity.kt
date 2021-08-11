3839f000b00cfef5bf8a99f2a4d82e4be42f85c6
Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2021-08-11 22:52:26 +0700

    add image to the Dice Roller app

--- DiceRoller/app/src/main/java/com/mazupi/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/mazupi/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mazupi.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,13 +14,29 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+//        val resultTextView: TextView = findViewById(R.id.textView)
+//        resultTextView.text = diceRoll.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResouce = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResouce)
+        diceImage.contentDescription = diceRoll.toString()
+
         Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
     }
 }
